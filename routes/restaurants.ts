d77ab023ed3b5a376371e7f6160a0b223f911d4a
Vladimir <volodya.mozgovykh@gmail.com>
2025-10-10 23:15:30 +0200

    Add zValidator

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,9 +1,16 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { RestaurantSchema } from "../schema";
 
 const app = new Hono();
 
 app.get("/", async (c) => {
   return c.text("Hello, test from restaurants");
+});
+
+app.post("/", zValidator("json", RestaurantSchema), async (c) => {
+  const validated = c.req.valid("json");
+  return c.text("Data validated and received");
 });
 
 export default app;
