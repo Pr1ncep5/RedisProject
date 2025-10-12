b7bf41a424bd58a3595b8e278a35690283aa813b
Vladimir <volodya.mozgovykh@gmail.com>
2025-10-12 21:16:14 +0200

    Add keys for cuisines

--- utils/keys.ts
+++ utils/keys.ts
@@ -6,3 +6,9 @@ export const restaurantKeyById = (id: string) => getKeyName("restaurants", id);
 export const reviewKeyById = (id: string) => getKeyName("reviews", id);
 export const reviewDetailsKeyById = (id: string) =>
   getKeyName("review_details", id);
+
+export const cuisinesKey = getKeyName("cuisines");
+export const cuisineKey = (cuisineName: string) =>
+  getKeyName("cuisine", cuisineName);
+export const restaurantCuisinesKeyById = (id: string) =>
+  getKeyName("restaurant_cuisines", id);
