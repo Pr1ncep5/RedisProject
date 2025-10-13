c2a5f07c3156daeb47748ef7e641bf1348a044b8
Vladimir <volodya.mozgovykh@gmail.com>
2025-10-13 19:01:56 +0200

    Add cuisines endpoints with Sets

--- routes/cuisines.ts
+++ routes/cuisines.ts
@@ -1,5 +1,29 @@
 import { Hono } from "hono";
+import { initializeRedisClient } from "../utils/client";
+import { cuisineKey, cuisinesKey, restaurantKeyById } from "../utils/keys";
+import { createSuccessResponse } from "../utils/responses";
 
 const app = new Hono();
+
+app.get("/", async (c) => {
+  const client = await initializeRedisClient();
+  const cuisines = await client.sMembers(cuisinesKey);
+
+  const responseBody = createSuccessResponse(cuisines);
+  return c.json(responseBody, 200);
+});
+
+app.get("/:cuisine", async (c) => {
+  const cuisine = c.req.param("cuisine");
+  const client = await initializeRedisClient();
+
+  const restaurantIds = await client.sMembers(cuisineKey(cuisine));
+  const restaurants = await Promise.all(
+    restaurantIds.map((id) => client.hGet(restaurantKeyById(id), "name"))
+  );
+
+  const responseBody = createSuccessResponse(restaurants);
+  return c.json(responseBody, 200);
+});
 
 export default app;
